0dc21124cbe966f29a66a016c2a9083cbd458704
pi <wanbicoi123@gmail.com>
2024-05-04 21:10:33 +0700

    fixed

--- src/Task 1.2/TermFrequencyFilter.java
+++ src/Task 1.2/TermFrequencyFilter.java
@@ -2,51 +2,99 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.io.*;
 
 public class TermFrequencyFilter {
+    public static class PairWritable implements Writable {
+
+        private Text docId;
+        private DoubleWritable frequency;
+
+        public PairWritable() {
+            this.docId = new Text();
+            this.frequency = new DoubleWritable();
+        }
+        public PairWritable(String docId, double frequency) {
+            this.docId = new Text(docId);
+            this.frequency = new DoubleWritable(frequency);
+        }
+
+        public void set(String docId, double frequency) {
+            this.docId = new Text(docId);
+            this.frequency = new DoubleWritable(frequency);
+        }
+
+        public Text getDocId() {
+            return docId;
+        }
+
+        public DoubleWritable getFrequency() {
+            return frequency;
+        }
+
+        @Override
+        public void write(DataOutput out) throws IOException {
+            docId.write(out);
+            frequency.write(out);
+        }
+
+        @Override
+        public void readFields(DataInput in) throws IOException {
+            docId.readFields(in);
+            frequency.readFields(in);
+        }
+
+        @Override
+        public String toString() {
+            return docId.toString() + " " + frequency.toString();
+        }
+    }
 
     public static class TokenizerMapper
-            extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+            extends Mapper<Object, Text, Text, PairWritable> {
 
         private Text termId = new Text();
-        private DoubleWritable frequency = new DoubleWritable();
+        private PairWritable pair = new PairWritable();
 
-        public void map(LongWritable key, Text value, Context context
+        public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            if(key.get() == 0 || key.get() == 1) {
-                return;
-            }
             String[] parts = value.toString().split("\\s+");
             if (parts.length == 3) {
                 termId.set(parts[0]);
-                frequency.set(Double.parseDouble(parts[2]));
-                context.write(termId, frequency);
+                pair.set(parts[1],Double.parseDouble(parts[2]));
+                context.write(termId, pair);
             }
         }
     }
 
     public static class FrequencySumReducer
-            extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+            extends Reducer<Text, PairWritable, Text, PairWritable> {
 
-        private DoubleWritable result = new DoubleWritable();
-
-        public void reduce(Text key, Iterable<DoubleWritable> values,
-                Context context
+        public void reduce(Text key, Iterable<PairWritable> values,
+                           Context context
         ) throws IOException, InterruptedException {
             double sum = 0.0;
-            for (DoubleWritable val : values) {
-                sum += val.get();
+            List<PairWritable> cache = new ArrayList<>();
+
+            for (PairWritable val : values) {
+                sum += val.getFrequency().get();
+                cache.add(new PairWritable(val.getDocId().toString(), val.getFrequency().get()));
             }
             if (sum >= 3.0) {
-                result.set(sum);
-                context.write(key, result);
+                for (int i = 0; i < cache.size(); ++i) {
+                    context.write(key, cache.get(i));
+                }
             }
         }
     }
@@ -59,7 +107,7 @@ public class TermFrequencyFilter {
         job.setCombinerClass(FrequencySumReducer.class);
         job.setReducerClass(FrequencySumReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(DoubleWritable.class);
+        job.setOutputValueClass(PairWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
